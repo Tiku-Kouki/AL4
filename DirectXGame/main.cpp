e6dd04565676f94cb93e6c2e629a9a973684bb5f
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-10-06 15:58:30 +0900

    01_01確認課題

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2C_19_チク_コウキ_AL4");
+	win->CreateGameWindow(L"LE2C_17_チク_コウキ_AL4");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
