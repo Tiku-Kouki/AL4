6524334f5b264dd2a7aec6e158db22c407fd435f
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-11-10 15:35:24 +0900

    01_05途中

---
+++ Player.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#include "Affine.h"
+#include "Input.h"
+#include "Model.h"
+#include "WorldTransform.h"
+#include "ViewProjection.h"
+#include <list>
+#include "Windows.h"
+#include "FollowCamera.h"
+#include "BaseCharacter.h"
+
+class Player : public BaseCharacter {
+
+ public:
+
+	void Initalize(const std::vector<Model*>& models) override;
+
+
+	void Update() override;
+
+
+
+	void Draw(ViewProjection& viewProjection) override;
+
+	Vector3 GetWorldPosition();
+
+	const WorldTransform& GetWorldTransform() { return worldTransform_[0]; }
+
+	void SetViewProjection(const ViewProjection* viewProjection) {viewProjection_ = viewProjection;}
+
+	//浮遊ギミック初期化
+	void InitializeFloatingGimmick();
+	//浮遊ギミック更新
+	void UpdateFloatingGimmick();
+
+private:
+
+	WorldTransform worldTransform_[4];
+
+	Model* model_ = nullptr;
+
+	// 3Dモデル
+	Model* modelFighterBody_ = nullptr;
+	Model* modelFighterHead_ = nullptr;
+	Model* modelFighterL_arm_ = nullptr;
+	Model* modelFighterR_arm_ = nullptr;
+
+
+	Input* input_ = nullptr;
+
+	uint32_t textureHandle_ = 0u;
+
+	//　カメラのビュープロジェクション
+	const ViewProjection* viewProjection_ = nullptr;
+
+	//浮遊ギミックの媒介変数
+	float floatingParameter_ = 0.0f;
+
+
+
+
+	};
